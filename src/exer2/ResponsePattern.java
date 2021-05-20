ed027b89c7054db381ab81a94f2cd08a96f757b4
Rashoru-Infinity <65536toaru@gmail.com>
2021-05-20 06:12:37 +0000

    deleted:    src/exer2/Main.java 	deleted:    src/exer2/ReadLines.java 	deleted:    src/exer2/ResponsePattern.java

--- src/exer2/ResponsePattern.java
+++
@@ -1,8 +0,0 @@
-public class ResponsePattern {
-	public String keyWord;
-	public String response;
-	public RespoinsePattern(String keyWord, String response) {
-		this.keyWord = keyWord;
-		this.response = response;
-	}
-}
